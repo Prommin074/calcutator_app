2618d1e00d313f22b489370adcda2e8b7dd1fa5b
unknown <minza14578@gmail.com>
2025-05-18 12:57:47 +0700

    fix title

--- calculatorapp.py
+++ calculatorapp.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox,ttk
 
 GUI = Tk()
-GUI.title("Calculator")
+GUI.title("Calculator_app")
 GUI.geometry("500x800")
 
 L = Label(GUI, text="Calculator", font=("Arial", 30), fg="black")
