f70ad50ad5f1ea908d34a4f1aea3a38e9cf481c1
unknown <minza14578@gmail.com>
2025-05-19 12:53:52 +0700

    add cal_func

--- calculatorapp.py
+++ calculatorapp.py
@@ -6,10 +6,23 @@ GUI.title("Calculator_app")
 GUI.geometry("500x800")
 
 L = Label(GUI, text="Calculator", font=("Arial", 30), fg="black")
-L1 = Label(GUI, text="User1", font=("Arial", 30), fg="black")
-L2 = Label(GUI, text="User2", font=("Arial", 30), fg="black")
 L.pack()
-L1.pack()
-L2.pack()
 
+x1 = StringVar()
+e1 = ttk.Entry(GUI, textvariable=x1, font=("Arial", 20), width=10)
+e1.pack()
+
+x2 = StringVar()
+e2 = ttk.Entry(GUI, textvariable=x2, font=("Arial", 20), width=10)
+e2.pack()
+
+def cal():
+    y = float(x1.get()) * float(x2.get())
+    z1 = x1.get()
+    z2 = x2.get()
+    result = f"{z1} * {z2} = {y}"
+    messagebox.showinfo("Result", result)
+
+b1 = Button(GUI, text="Calculate Now!!!" , font=("Arial", 50), command=cal)
+b1.pack()
 GUI.mainloop()
